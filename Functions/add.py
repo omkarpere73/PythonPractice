42c9ea41f43e0435ac4b1fda30d6b3c24eedb5d9
omkarpere73 <omkarpere7363@gmail.com>
2026-05-21 20:48:52 +0530

    Added new function practice files

---
+++ Functions/add.py
@@ -0,0 +1,5 @@
+def add(x, y):
+    return x + y
+
+result = add(5 , 8)
+print(result)
---
+++ Functions/odd_even.py
@@ -0,0 +1,10 @@
+def odd_even(x):
+    if x % 2 == 0 :
+        return "even"
+    else:
+        return "odd"
+
+print(odd_even(6))
+
+
+
